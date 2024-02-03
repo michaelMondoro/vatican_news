5b2701fe83bb06eb9657028c78248f04ad304712
Michael Mondoro <38412172+michaelMondoro@users.noreply.github.com>
2024-02-03 13:49:52 -0500

    added ids, fixed sorting and reset articles on each get_news() call

--- vatican_news.py
+++ vatican_news.py
@@ -1,6 +1,7 @@
 import requests
 import bs4 as bs
 from dateutil import parser
+import uuid
 
 class VaticanNews():
     def __init__(self):
@@ -10,29 +11,29 @@ class VaticanNews():
 
 
     def get_news(self):
+        self.articles = []
         xml = requests.get(self.news_url)
         soup = bs.BeautifulSoup(xml.content, "xml")
         items = soup.find_all('item')
         for item in items:
             self.articles.append(Item(item.find('title').text, item.find('pubDate').text, desc=item.find('description').text, link=item.find("link").text))
 
-        self.articles.sort(key=lambda x: x.pubDate, reverse=True)
-
         return self.articles
 
 
 class Item:
     def __init__(self, title, pubDate, desc=None, link=None):
+        self.id = str(uuid.uuid4())
         self.title = title.strip()
         self.link = link
         self.pubDate = parser.parse(pubDate.strip())
-        self.pubDate = self.pubDate.strftime("%b %d, %Y")
+        self.pubDate = self.pubDate.strftime("%b %d, %Y %I:%M%p")
 
         desc = desc.strip()
         start = desc.index('<p>')+3
         end = desc.index('</p>')
         self.desc = desc[start:end]
-    
+
     def __str__(self):
         return f"Item(title={self.title}, pubDate={self.pubDate.ctime()}, desc=[ {self.desc} ], link={self.link})"
     def __repr__(self):
